0e4beb9b425198807171b16b594dfd193d55dafd
J4qu3s <jarkko.hoyhtya@gmail.com>
2022-05-31 21:50:46 +0300

    at43doingSvg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/index';
+import Home from './components/Home';
 
 // https://www.youtube.com/watch?v=bmpI252DmiI
-// at 22 
+// at 43 - doing svg logo 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />}/>  
+        </Route>
       </Routes>
     </>
   );
---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import { faHelicopterSymbol } from '@fortawesome/free-solid-svg-icons';
+import './index.scss';
+
+const Logo = () => {
+    return (
+        <div className="logo-container">
+            <img src="" alt="" className="solid-logo" />
+        </div>
+    )
+}
+
+export default Logo;
